1bdd2daa95963c5c489452d97107a4bcb58f9819
RakeshBatra <raakesh.batra@rediffmail.com>
2019-10-23 03:00:14 +0000

    ColtBringup
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- colt_oneplus3.mk
+++ colt_oneplus3.mk
@@ -20,13 +20,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from oneplus3 device
 $(call inherit-product, device/oneplus/oneplus3/device.mk)
 
-# Inherit some common Nitrogen stuff.
-$(call inherit-product, vendor/nitrogen/products/common.mk)
+# Inherit some common ColtOS stuff.
+$(call inherit-product, vendor/colt/config/common.mk)
+
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080x1920
 
 # Vendor security patch level
 VENDOR_SECURITY_PATCH := 2019-04-01
 
-PRODUCT_NAME := nitrogen_oneplus3
+PRODUCT_NAME := colt_oneplus3
 PRODUCT_DEVICE := oneplus3
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := OnePlus
@@ -37,3 +40,5 @@ PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 BUILD_FINGERPRINT := "OnePlus/OnePlus3/OnePlus3:9/PKQ1.181203.001/1907311932:user/release-keys"
 
 TARGET_VENDOR := oneplus
+
+COLT_BUILD_TYPE := Official
